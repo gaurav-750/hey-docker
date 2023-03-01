9748e2d72eac69df9c4a5b51f79a083c27aa67d2
Gaurav <gauravsomani52750@gmail.com>
2023-03-01 21:44:57 +0530

    pushed version 2 in docker hub

--- hey-flask/index.py
+++ hey-flask/index.py
@@ -8,5 +8,23 @@ def run():
     return "{\"message\": \"Hey there python\"}"
 
 
+@helloworld.route('/api')
+def run_api():
+    return "<h1> Hey there, it's API!"
+
+
 if __name__ == '__main__':
     helloworld.run(host="0.0.0.0", port=int("3000"), debug=True)
+
+
+# app = Flask(__name__)
+
+
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+
+# @app.route("/api")
+# def hello_api():
+#     return "<h1>Hey there, it's API!</h1>"
